9f8b48d97d1be7402d3e5a948fda27cce3d239be
salah <salah@gmail.com>
2023-05-13 15:34:00 +0530

    test

--- script.groovy
+++ script.groovy
@@ -1,1 +1,20 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t sulemanfazal12/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push sulemanfazal12/demo-app:jma-2.0'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
 
