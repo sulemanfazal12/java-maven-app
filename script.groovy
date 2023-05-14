596a8d16f721113ca123cb9f78b434726d114b5d
s suleman <122139623+sulemanfazal12@users.noreply.github.com>
2023-05-14 15:25:16 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def testApp() {
 }
 
 def deployApp() {
-    echo "deploying the application..."
+    echo "deploying the application...#############"
     echo "deploying version ${params.VERSION}..."
 }
 
