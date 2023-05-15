1f230532f7d0eafe3b7e92a0ac67dbaaf85c11bc
s suleman <122139623+sulemanfazal12@users.noreply.github.com>
2023-05-15 06:18:09 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,5 @@
-def buildApp() {
-    echo "building the application..."
-}
-
-def testApp() {
-    echo "Test the application"
-}
-
 def deployApp() {
-    echo "deploying the application...#############"
-    echo "deploying version ${params.VERSION}..."
+    echo 'deploying the application...'
 }
 
 return this
